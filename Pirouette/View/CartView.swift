1993cd1ff2d627bcd24fd656e3e7afd412c0ef1a
Ihordolhalov1 <servicegambro@gmail.com>
2024-03-31 13:45:52 +0100

    Some more minor changes

--- Pirouette/View/CartView.swift
+++ Pirouette/View/CartView.swift
@@ -14,10 +14,15 @@ struct CartView: View {
 
     @State private var isAlertPresented = false
     @State private var isAddressEmpty = false
+    @State private var isNameOrPhoneEmpty = false
+
     @State private var deliveryPicker = 0
     @State private var countOfPeople = 1
     @State private var address = "  "
     @State private var addressField = ""
+    @State private var clientName = ""
+    @State private var clientPhone = ""
+
     @State private var selectedTime = Date()
 
     var body: some View {
@@ -25,7 +30,7 @@ struct CartView: View {
         VStack {
             Text("Cart").font(.title)  .fontWeight(.bold)
                 .padding()
-                            
+            
             List(viewModel.positions) { position in
                 PositionCell(position: position)
                     .swipeActions {
@@ -39,14 +44,31 @@ struct CartView: View {
                         }.tint(.red)
                     }
             }.listStyle(.plain)
-                .onChange(of: viewModel.positions.count) {  newValue in 
+                .onChange(of: viewModel.positions.count) {  newValue in
                     
                     viewModel.countOfPositions = viewModel.positions.count
                     numberOfDishes = viewModel.countOfPositions
                 }
                 .onAppear() {
                     numberOfDishes = viewModel.countOfPositions
-
+                   
+                    // Витягую дані по користувачу
+                    
+                    DatabaseService.shared.getProfile(by: AuthService.shared.currentUser!.uid) { result in
+                        switch result {
+                        case .success(let res):
+                            addressField =  res.address
+                            clientName = res.name
+                            clientPhone = res.phone
+                            
+                        case .failure(let error):
+                            print("ПОМИЛКА НА ОТРИМАННІ ДЕФОЛТНОЇ АДРЕСИ")
+                            print(error.localizedDescription)
+                        }
+                    }
+                    
+                    
+                    
                 }
             
             if viewModel.positions.count > 0 {
@@ -57,14 +79,14 @@ struct CartView: View {
                     Text("Delivery").tag(3)
                     
                     
-                } 
+                }
                 .pickerStyle(SegmentedPickerStyle())
                 .padding()
                 
                 .onChange(of: deliveryPicker) {  newValue in
                     switch deliveryPicker {
                     case 1:
-                        address = "Book a table for \(countOfPeople)"
+                        address = "Thank you! We will book a table for \(countOfPeople) person"
                     case 2:
                         address = "Pick up"
                     case 3:
@@ -83,14 +105,16 @@ struct CartView: View {
                             }
                     }.padding(.horizontal).font(.subheadline.bold())
                     
-                    Text ("We will book a table for \(countOfPeople)")
+                    Text("We will book a table for \(countOfPeople) person")
                 case 2:
-                    Text("We will prepare your order for pick up").padding()
+                    Text("We will prepare your order for pick up from our restaurant")
+                        .lineLimit(nil)
+                        .padding()
                 case 3:
                     HStack {
                         TextField("Write your address here", text: $addressField)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .onAppear() {
+                          /*  .onAppear() {
                                 DatabaseService.shared.getProfile(by: AuthService.shared.currentUser!.uid) { result in
                                     switch result {
                                     case .success(let res):
@@ -100,7 +124,7 @@ struct CartView: View {
                                         print(error.localizedDescription)
                                     }
                                 }
-                            }
+                            } */
                             .onChange(of: addressField) {  newValue in
                                 address = "We will deliver to address: \(addressField)"
                             }
@@ -110,26 +134,52 @@ struct CartView: View {
                     
                 default:
                     Text("Please  choose how you would like to get your order")
+                        .lineLimit(nil)
+
                 }
                 
-                DatePicker(selection: $selectedTime, in: Date()..., displayedComponents: .hourAndMinute) {
-                    Text("Select time").padding()
+              /*  DatePicker(selection: $selectedTime, in: Date()..., displayedComponents: .hourAndMinute) {
+                    Text("Please select time").padding()
                 }
                 .datePickerStyle(CompactDatePickerStyle())
                 .padding()
                 .onAppear {
-                    UIDatePicker.appearance().minuteInterval = 10
+                    UIDatePicker.appearance().minuteInterval = 15
                     
-                    let minuteInterval = 10
+                    let minuteInterval = 15
                     let calendar = Calendar.current
                     let currentMinute = calendar.component(.minute, from: selectedTime)
                     let roundedMinute = (currentMinute / minuteInterval) * minuteInterval
                     let roundedDate = calendar.date(bySetting: .minute, value: roundedMinute, of: selectedTime)!
                     selectedTime = roundedDate
-                }
-            }
+                */
+                let currentDate = Date()
+                let calendar = Calendar.current
+                let currentDatePlus30Minutes = calendar.date(byAdding: .minute, value: 30, to: currentDate)!
+                let closingTime = calendar.date(bySettingHour: 20, minute: 0, second: 0, of: currentDate)!
 
+                DatePicker(selection: $selectedTime, in: currentDatePlus30Minutes...closingTime, displayedComponents: .hourAndMinute) {
+                    Text("Please select time").padding()
+                }
+                .datePickerStyle(CompactDatePickerStyle())
+                .padding()
+                .onAppear {
+                    UIDatePicker.appearance().minuteInterval = 15
+                    
+                    let minuteInterval = 15
+                    let currentMinute = calendar.component(.minute, from: selectedTime)
+                    let roundedMinute = (currentMinute / minuteInterval) * minuteInterval
+                    let roundedDate = calendar.date(bySetting: .minute, value: roundedMinute, of: selectedTime)!
+                    selectedTime = roundedDate
+                }
+
+
+                    
+                    
+            }
             
+               
+        
             HStack{
                 Text("Total: ").fontWeight(.bold)
                 Spacer()
@@ -154,28 +204,43 @@ struct CartView: View {
                     Spacer()
                     
                     Button(action: {
-                        if addressField.count < 5 {
-                            self.isAddressEmpty.toggle()
+                     /*   DatabaseService.shared.getProfile(by: AuthService.shared.currentUser!.uid) { result in
+                            switch result {
+                            case .success(let res):
+                                clientName = res.name
+                                clientPhone =  res.phone
+                            case .failure(let error):
+                                print("ПОМИЛКА НА ОТРИМАННІ ДЕФОЛТНОГО IМʼЯ та НОМЕРА ТЕЛЕФОНУ")
+                                print(error.localizedDescription)
+                            }
+                        } */
+                    
+                        if clientName.count < 2 || clientPhone.count < 6 {
+                            isNameOrPhoneEmpty.toggle()
                         } else {
                             
-                            isAlertPresented.toggle()
-                            print("ЗНАЧЕНИЕ isAlertPresented \(isAlertPresented) ")
-                            var order = Order(userID: AuthService.shared.currentUser!.uid, date: Date(), status: OrderStatus.new.rawValue, address: address, dateToGet: selectedTime)
-                            order.positions = self.viewModel.positions
-                            
-                            
-                            DatabaseService.shared.setOrder(order: order) { result in
-                                switch result{
-                                case .success(let order):
-                                    print(order.cost)
-                                case .failure(let error):
-                                    print(error.localizedDescription)
+                            if deliveryPicker == 3 && addressField.count < 5 {
+                                self.isAddressEmpty.toggle()
+                            } else {
+                                
+                                isAlertPresented.toggle()
+                                print("ЗНАЧЕНИЕ isAlertPresented \(isAlertPresented) ")
+                                var order = Order(userID: AuthService.shared.currentUser!.uid, date: Date(), status: OrderStatus.new.rawValue, address: address, dateToGet: selectedTime)
+                                order.positions = self.viewModel.positions
+                                
+                                
+                                DatabaseService.shared.setOrder(order: order) { result in
+                                    switch result{
+                                    case .success(let order):
+                                        print(order.cost)
+                                    case .failure(let error):
+                                        print(error.localizedDescription)
+                                    }
                                 }
+                                viewModel.positions.removeAll()
                             }
-                            viewModel.positions.removeAll()
+                            
                         }
-                        
-                        
                     }, label: {
                         
                         CustomButton(text: "Accept", opacity: 1.0)
@@ -211,20 +276,16 @@ struct CartView: View {
                          Text("Please let us know the shipping address")
                      }
                  )
-        
-        
-        
-        /*     .alert(isPresented: $isAlertPresented) {
-                    Alert(title: Text("Thank you"), message:
-                    Text("Order was accepted")
-                , dismissButton: .default(Text("OK")))
-                }
-        
-                .alert(isPresented: $isAddressEmpty) {
-                    Alert(title: Text("Address is unknown"), message:
-                    Text("Please define shipping address here or default address on Account page")
-                , dismissButton: .default(Text("OK")))
-                }*/
+                 .alert(
+                     "Your name ot phone number is undefined, please fill in the Account page",
+                     isPresented: $isNameOrPhoneEmpty,
+                     actions: {
+                         Button("Close") {}
+                     },
+                     message: {
+                         Text("Please let us know your name and phone number")
+                     }
+                 )
     }
 }
 
